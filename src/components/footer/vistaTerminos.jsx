090622e90e36ea34c339158137173da399fafa5d
Juan ignacio yone <juan.9607@hotmail.com>
2021-11-02 17:09:47 -0300

    terminos

--- src/components/footer/vistaTerminos.jsx
+++ src/components/footer/vistaTerminos.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
-export const VistaTerminosCondiciones = () => {
-    return (
-        <>
-            
+export const VistaTerminosCondiciones = () => (
+    <div className='container' style={{ marginTop: '80px', marginLeft: '25px', marginRigth: '25px' }}>
+        <h5>GENERAL</h5>
+        <p>A continuación se encuentran los Términos y Condiciones (en adelante, los “Términos y Condiciones”) sobre los cuales se basará la relación entre Pedilo Ya  S.A. (en adelante, Pedilo Ya ) y toda persona que utilice los servicios de Pedilo Ya  (en adelante, el “Usuario”).
+            Cualquier persona que no acepte estos Términos y Condiciones, los cuales tienen un carácter obligatorio y vinculante, deberá abstenerse de utilizar los servicios de Pedilo Ya .
+            Con su registro y utilización de los servicios de Pedilo Ya , los Usuarios están manifestando su aceptación, expresa e inequívoca de los Términos y Condiciones.
+            Si los Usuarios tienen dudas respecto a estos Términos y Condiciones pueden comunicarse con nuestro equipo de Atención al Cliente a través del chat disponible en nuestro Portal o al correo electrónico contacto@pediloya .com.
+        </p>
+        <h5>DEFINICIONES</h5>
+        <p>"Aplicación", hace referencia a la aplicación Pedilo Ya  disponible para las tecnologías móviles.
+            "Bienes", son los bienes, productos o servicios que pueden llegar a ser ofrecidos por los Oferentes a través del Portal e integran el Pedido del Usuario. Los Bienes son suministrados por los Oferentes, siendo responsables de los mismos ante los Usuarios.
+            "Nosotros", "Nuestro", y "Pedilo Ya ", siempre que se haga referencia a los vocablos, se está haciendo referencia directa a la sociedad Pedilo Ya . S.A., identificación fiscal 30-71198576-6, con domicilio en Mariscal Antonio José de Sucre 1530, Piso 6, Ciudad de Buenos Aires, República Argentina y sus sociedades vinculadas.
+            "Oferente", se refiere a agentes externos y/o terceros ajenos a Pedilo Ya , que previamente han contratado con Pedilo Ya  sus servicios de intermediación, aportando toda la información de los Bienes que se exhiben a través del Portal (precio, características, y en general todas sus condiciones objetivas). Estos, en su calidad exclusiva de productor, proveedor y/o expendedor, son los directamente encargados de cumplir con todas las características objetivas del producto y/o servicio publicado en el Portal.
+            "Pedido" hace referencia a la solicitud del Usuario a través del Portal de los Bienes de Oferentes.
+            “Portal” hace referencia nuestra Aplicación y/o al Sitio Web a través de las cuales, en calidad de intermediarios, facilitamos el contacto entre Oferentes, Usuarios y repartidores.
+            "Usuario" hace referencia a todas las personas humanas o jurídicas que accedan a nuestro Portal y realizan Pedidos sea a través del Sitio Web o de la Aplicación.
+            "Servicio" hace referencia al servicio de intermediación solicitado por el Usuario a través del Portal.
+            "Sitio Web" hace referencia al sitio web www.Pedilo Ya .com.ar.
+            En caso de utilizarse alguna de estas palabras definidas en plural, tendrán el mismo significado que el indicado en el presente.
+        </p>
+        <h5>CAPACIDAD</h5>
+        <p>Los Servicios solo están disponibles para Usuarios que tengan capacidad legal para contratar y sean mayores de edad.
 
-        </>
+            Para registrar una persona jurídica como Usuario, se deberá contar con la representación legal suficiente como para contratar a nombre y representación de la misma como así también de obligar a la misma según estos Términos y Condiciones.
+
+            En caso de desear adquirir bebidas alcohólicas y/o tabaco y/o medicamentos, en pleno cumplimiento de las normas aplicables, el Usuario deberá acreditar ser mayor de 18 años, exhibiendo una identificación al momento de la entrega. Asimismo, en el caso de venta de bebidas alcohólicas pueden resultar aplicables restricciones horarias en función de la normativa local del lugar de residencia del Usuario, adicionalmente, se le podrá pedir una fotografía de la identificación con la cual se acreditó la mayoría de edad.
+        </p>
+       
+        <h5>CREACIÓN DE CUENTA</h5>
+        <p>Para el uso del Portal, los Usuarios deberán crear una cuenta de usuario (en adelante, la “Cuenta”) donde se le solicitarán ciertos datos personales tales como: nombre y apellido, fecha de nacimiento, dirección, teléfono, correo electrónico y datos para procesar los pagos online (en adelante, los “Datos”). Los Usuarios garantizan y responden, en cualquier caso, de la veracidad, exactitud, vigencia, autenticidad y actualización de sus Datos. En ningún caso Pedilo Ya  se responsabiliza por la certeza de los Datos de los Usuarios.
+            Pedilo Ya  podrá requerir alguna información o documentación adicional a los efectos de comprobar o corroborar los Datos, pudiendo suspender temporal o definitivamente a aquellos Usuarios cuyos Datos no hayan podido ser confirmados.
+            Para acceder a su Cuenta personal, el Usuario deberá ingresar su correo electrónico y una contraseña la cual deberá mantener siempre de manera confidencial. Si el Usuario olvida su contraseña, podrá restablecerla haciendo clic en “Olvidé mi contraseña”.
+            La Cuenta es única e intransferible, estando prohibido que un Usuario registre o tenga más de una Cuenta por correo electrónico o por celular validado, por lo que de detectarse tal incumplimiento, Pedilo Ya  se reserva el derecho de cancelar, suspender o inhabilitar las cuentas.
+            El Usuario es responsable del resguardo de sus datos de registro y contraseña, así como de todas las transacciones realizadas en su Cuenta, debiendo notificar a Pedilo Ya  inmediatamente, de forma fehaciente, cualquier uso no autorizado de la misma, así como cualquier sustracción, divulgación o pérdida de sus datos de acceso al Portal. Pedilo Ya  vela por la protección de los datos de los Usuarios. Sin embargo, no será responsable del uso fraudulento que puedan hacer terceros de la Cuenta del Usuario, incluidos usos indebidos de sus datos asociados a los pagos online.
+            Los Usuarios tienen la facultad de ejercer el derecho de acceso, en cualquier momento y sin restricciones, de sus datos personales. Pedilo Ya  tiene la atribución de atender las denuncias y reclamos que se interpongan con relación al incumplimiento de las normas sobre protección de datos personales.
+        </p>
+        <p><b>Queremos dejar acentado que todo lo que concerniente a este sitio web es falso, tratado a modo de DEMO como una copia de algunas funcionalidades y visuales del sitio "PedidosYa.com" sumado a algunas funcionalidades propias agregadas por el equipo de trabajo con una finalidad netamente academica y como una forma de llevar a la practica habilidades de desarrollo adquiridas a lo largo de la carrera Licenciatura en sistemas de la UNLa </b></p>
+    </div>
 
 
-    )
-}
+)
